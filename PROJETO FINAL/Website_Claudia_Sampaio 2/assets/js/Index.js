5e5618754d48352bb91cfae7ff19a747f0144ec5
lau81s <86448914+lau81s@users.noreply.github.com>
2021-06-25 01:48:26 +0100

    Add files via upload

---
+++ PROJETO FINAL/Website_Claudia_Sampaio 2/assets/js/Index.js
@@ -0,0 +1,7 @@
+function initMap() {
+  new google.maps.Map(document.getElementById("map"), {
+    mapId: "96957fec1ddeed8d",
+    center: { lat: -19.49583,, lng: -41.06389  },
+    zoom: 9,
+  });
+}
